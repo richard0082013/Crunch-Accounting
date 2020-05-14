79110cb98703bf41dc92c331e8d77f922d219e2a
Bo Tian <richard0082013@gmail.com>
2020-05-14 13:39:27 +1000

    fix validation issue

--- src/App.js
+++ src/App.js
@@ -41,10 +41,10 @@ function App() {
         }}
         validationSchema={Yup.object({
           firstName: Yup.string().required('*Required'),
-          postCode: Yup.string().min(4, "Only 4 digitals").matches(/^(?:(?:[2-8]\d|9[0-7]|0?[28]|0?9(?=09))(?:\d{2}))$/, "Is not an Australian Postcode"),
+          postCode: Yup.string().min(4, "Only 4 digitals").matches(/^(?:(?:[2-8]\d|9[0-7]|0?[28]|0?9(?=09))(?:\d{2}))$/, "*Is not an Australian Postcode"),
           lastName: Yup.string().required('*Required'),
           accountName: Yup.string().required('*Required'),
-          phone: Yup.string().matches(/^\D*0(\D*\d){9}\D*$/, "Invalid phone number").required('*Required'),
+          phone: Yup.string().matches(/^\D*0(\D*\d){9}\D*$/, "*Invalid phone number, 10 digitals start with 0").required('*Required'),
           email: Yup.string().email('*Invalid email address').required('*Required'),
           street: Yup.string().required('*Required'),
           city: Yup.string().required('*Required'),
